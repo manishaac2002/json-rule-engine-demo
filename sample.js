e5200b307e5ba9592c91a85cd438fb293d623b96
manishaac2002 <manishaac2002@gmail.com>
2023-08-11 12:13:28 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,32 @@
+import {Engine} from 'json-rules-engine'
+
+ const start =async()=>{
+//setup a new engine
+    const engine = new Engine()
+
+    engine.addRule({
+        // define the condition for when result should display
+        conditions :{
+            all:[{
+                fact:'displayMessage',
+                operator:'equal',
+                value:true
+            }],
+        },
+        event:{
+            type:'message',
+            params:{
+                data:'Hello world'
+            }
+        }
+    })
+    
+    const facts ={displayMessage :true}
+    
+    //to run the engine
+    const  {events} = await engine.run(facts)
+
+    events.map(event=>console.log(event.params.data))
+
+}
+ start()
